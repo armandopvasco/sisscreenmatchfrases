1b91539a80cf0a82e04939969febc671f7239839
armando.vasconcelos <armando.vasconcelos@mv.com.br>
2026-09-14 01:11:21 -0300

    complementando

--- src/main/java/br/com/alura/screenmatchfrases/screenmatchfrases/repository/FraseRepository.java
+++ src/main/java/br/com/alura/screenmatchfrases/screenmatchfrases/repository/FraseRepository.java
@@ -7,6 +7,6 @@ import org.springframework.data.jpa.repository.Query;
 
 public interface FraseRepository extends JpaRepository<Frase, Long> {
     //@Query("SELECT f FROM Frase f order by function('RANDOM') LIMIT 1")
-    @Query(value = "SELECT * FROM frases ORDER BY RANDOM() LIMIT 1", nativeQuery = true)
+    @Query(value = "SELECT * FROM public.frases ORDER BY RANDOM() LIMIT 1", nativeQuery = true)
     Frase obtemFrase();
 }
